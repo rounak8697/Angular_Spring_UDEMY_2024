1aa235977a24f389ecf1ecb4a99132d7d3c44f93
demoFor1stTime <rounakghosh063@gmail.com>
2024-02-24 18:27:33 +0530

    ngClass Added
    for condition and row highlighted the rows based on condition

--- Angular/sales-project/src/app/sales-person-list/sales-person-list.component.ts
+++ Angular/sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -10,8 +10,8 @@ export class SalesPersonListComponent {
   //create an array of Salesperson
   salesPersonList: SalesPerson[] = [
     new SalesPerson('Rounak', 'Das', 'rounak@gmail.fr', 1000),
-    new SalesPerson('Rounak', 'Das', 'rounak@gmail.fr', 5000),
-    new SalesPerson('Rounak', 'Das', 'rounak@gmail.fr', 2050),
-    new SalesPerson('Rounak', 'Das', 'rounak@gmail.fr', 1950),
+    new SalesPerson('Rohit', 'Roy', 'rohit@gmail.fr', 5000),
+    new SalesPerson('Soumalya', 'Paul', 'soumalya@gmail.fr', 2050),
+    new SalesPerson('Abir', 'Rahman', 'abir@gmail.fr', 1950),
   ];
 }
